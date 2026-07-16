fb43d74561b0ca9982bd86d03797bf7907837f56
PatricioPorrasM <patricio_porras@hotmail.com>
2026-07-16 01:13:21 -0500

    nombres correctos de los notebooks

--- run_all.sh
+++ run_all.sh
@@ -4,21 +4,21 @@ set -e
 
 source .venv/bin/activate
 
-papermill notebooks/01_carga.ipynb \
-           notebooks/output/01_carga.ipynb
+papermill notebooks/01_carga_datos.ipynb \
+           notebooks/output/01_carga_datos.ipynb
 
 papermill notebooks/02_preprocesamiento_entrenamiento.ipynb \
            notebooks/output/02_preprocesamiento_entrenamiento.ipynb
 
-papermill notebooks/03_xxx.ipynb \
-           notebooks/output/03_xxx.ipynb
+papermill notebooks/03_evaluacion_comparativa.ipynb \
+           notebooks/output/03_evaluacion_comparativa.ipynb
 
-papermill notebooks/04_xxx.ipynb \
-           notebooks/output/04_xxx.ipynb
+papermill notebooks/04_explicabilidad_xai.ipynb \
+           notebooks/output/04_explicabilidad_xai.ipynb
 
-papermill notebooks/05_xxx.ipynb \
-           notebooks/output/05_xxx.ipynb
+papermill notebooks/05_estabilidad_temporal_regional.ipynb \
+           notebooks/output/05_estabilidad_temporal_regional.ipynb
 
-papermill notebooks/06_xxx.ipynb \
-           notebooks/output/06_xxx.ipynb
+papermill notebooks/06_contraste_teorico.ipynb \
+           notebooks/output/06_contraste_teorico.ipynb
            
